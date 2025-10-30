152c95659bcc9a380531d56229bdb5997b290741
lgh877 <lgh877@naver.com>
2025-10-31 03:30:16 +0900

    Added animateMob method by default in IStackableAnimationMob

--- src/main/java/com/crimsonsteve/crimsonsteveapi/interfaces/IStackableAnimationMob.java
+++ src/main/java/com/crimsonsteve/crimsonsteveapi/interfaces/IStackableAnimationMob.java
@@ -1,9 +1,16 @@
 package com.crimsonsteve.crimsonsteveapi.interfaces;
 
 import com.crimsonsteve.crimsonsteveapi.utils.animations.BaseAnimation;
+import net.minecraft.client.model.HierarchicalModel;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface IStackableAnimationMob {
     CopyOnWriteArrayList<? extends BaseAnimation> getAnimList();
+
+    default void animateMob(HierarchicalModel<?> hierarchicalModel, float ageInTicks, float partialTicks){
+        for (BaseAnimation anim : getAnimList()) {
+            anim.animate(hierarchicalModel, ageInTicks, partialTicks);
+        }
+    }
 }
