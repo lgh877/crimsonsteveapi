2156cde6f8508e027dcf3d264911cdd32a9f4946
lgh877 <lgh877@naver.com>
2025-11-30 03:21:50 +0900

    Fixed amplitude value of animation based rotation control methods a bit.

--- src/main/java/com/crimsonsteve/crimsonsteveapi/utils/animations/AnimUtil.java
+++ src/main/java/com/crimsonsteve/crimsonsteveapi/utils/animations/AnimUtil.java
@@ -3,7 +3,6 @@ package com.crimsonsteve.crimsonsteveapi.utils.animations;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.animation.KeyframeAnimations;
 import net.minecraft.client.model.HierarchicalModel;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.AnimationState;
 import org.joml.Vector3f;
 
@@ -24,8 +23,8 @@ public class AnimUtil {
     }
 
     public static void animationBasedYawControl(HierarchicalModel<?> model, AnimationDefinition lookLeft, AnimationDefinition lookRight, float headYaw){
-        if(headYaw > 0) KeyframeAnimations.animate(model, lookLeft, 1, headYaw, ANIMATION_VECTOR_CACHE);
-        else if(headYaw != 0) KeyframeAnimations.animate(model, lookRight, 1, -headYaw, ANIMATION_VECTOR_CACHE);
+        if(headYaw > 0) KeyframeAnimations.animate(model, lookRight, 1, headYaw, ANIMATION_VECTOR_CACHE);
+        else if(headYaw != 0) KeyframeAnimations.animate(model, lookLeft, 1, -headYaw, ANIMATION_VECTOR_CACHE);
     }
 
     public static void animationBasedPitchControl(HierarchicalModel<?> model, AnimationDefinition lookUp, AnimationDefinition lookDown, float headPitch){
