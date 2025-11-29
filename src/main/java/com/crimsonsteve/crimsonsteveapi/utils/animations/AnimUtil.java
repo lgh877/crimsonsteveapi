8d849163492ec2ad51dd8cf828dd43821c002b07
lgh877 <lgh877@naver.com>
2025-11-30 02:59:16 +0900

    Added animation based rotation control to AnimUtil

--- src/main/java/com/crimsonsteve/crimsonsteveapi/utils/animations/AnimUtil.java
+++ src/main/java/com/crimsonsteve/crimsonsteveapi/utils/animations/AnimUtil.java
@@ -3,6 +3,7 @@ package com.crimsonsteve.crimsonsteveapi.utils.animations;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.animation.KeyframeAnimations;
 import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.AnimationState;
 import org.joml.Vector3f;
 
@@ -20,5 +21,15 @@ public class AnimUtil {
         long i = (long) (limbSwing * 50.0F * speed);
         float f = limbSwingAmount * amplitude;
         KeyframeAnimations.animate(model, animDef, i, f, ANIMATION_VECTOR_CACHE);
+    }
+
+    public static void animationBasedYawControl(HierarchicalModel<?> model, AnimationDefinition lookLeft, AnimationDefinition lookRight, float headYaw){
+        if(headYaw > 0) KeyframeAnimations.animate(model, lookLeft, 1, headYaw, ANIMATION_VECTOR_CACHE);
+        else if(headYaw != 0) KeyframeAnimations.animate(model, lookRight, 1, -headYaw, ANIMATION_VECTOR_CACHE);
+    }
+
+    public static void animationBasedPitchControl(HierarchicalModel<?> model, AnimationDefinition lookUp, AnimationDefinition lookDown, float headPitch){
+        if(headPitch > 0) KeyframeAnimations.animate(model, lookDown, 1, headPitch, ANIMATION_VECTOR_CACHE);
+        else if(headPitch != 0) KeyframeAnimations.animate(model, lookUp, 1, -headPitch, ANIMATION_VECTOR_CACHE);
     }
 }
